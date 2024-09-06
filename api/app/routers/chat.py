781096720ce1158e253c5a25dd4313ce470c8da3
Alexander Ovechkin <klever1987@yandex.ru>
2024-09-06 11:17:07 +0000

    chat manager refactor

--- api/app/routers/chat.py
+++ api/app/routers/chat.py
@@ -2,8 +2,6 @@ from typing import List
 import json
 
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Depends
-from sqlalchemy import insert, select
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Session
 
 from ..repositories.messages_repository import MessagesRepository
@@ -11,27 +9,9 @@ from ..config.db_config import get_db
 from ..config.dependencies import get_messages_repository
 from ..models.models import Message
 from ..schemas.messages import MessageSchema
+from ..utils.chat_utils import ConnectionManager
 
 router = APIRouter()
-
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections: list[WebSocket] = []
-
-    async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append(websocket)
-
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
-
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
-
-    async def broadcast(self, message: MessageSchema):
-        for connection in self.active_connections:
-            await connection.send_json(message.to_dict())
-
 
 manager = ConnectionManager()
 
