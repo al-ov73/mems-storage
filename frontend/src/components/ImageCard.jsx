091f042e66609a25ba65ade8a6c4c2793bd34ad3
al-ov73 <klever1987@yandex.ru>
2024-09-23 12:56:00 +0000

    add heart component

--- frontend/src/components/ImageCard.jsx
+++ frontend/src/components/ImageCard.jsx
@@ -7,31 +7,26 @@ import { getMemes, deleteMeme } from '../utils/requests';
 import { setMemes } from "../slices/memesSlice";
 import React, { useState } from "react";
 import ImageModal from './ImageModal';
+import HeartComponent from './Heart';
 
 const ImageCard = ({ meme })  => {
   const [modalShow, setModalShow] = useState(false);
-  const access_token = localStorage.getItem('user')
-  const dispatch = useDispatch();
-  
-  const handleDelete = async (id, token) => {
-    const deleteResponse = await deleteMeme(id, token)
-    console.log('deleteResponse', deleteResponse);
-    const getMemesResponse = await getMemes(token);
-    console.log('getMemesResponse', getMemesResponse)
-    dispatch(setMemes(getMemesResponse.data))    
-  }
 
   return (
     <>
       <Card style={{ width: '12rem' }}>
+        
         <Image 
                 height="150rem"
                 src={meme.link}
                 className="rounded mx-auto mt-3 d-block "
                 alt='Картинка не загрузилась:('
                 onClick={() => setModalShow(true)}
-        />
-        <b align="center">{meme.name}</b>
+        /><span class="text-danger text-end"><HeartComponent/></span>
+        <span align="center">
+          {meme.name}
+        </span>
+        
       </Card>
       <ImageModal
         meme={meme}
