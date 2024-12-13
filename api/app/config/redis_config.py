9f32e32c6c6afa80e6a2bd056505f16d730ca471
al-ov73 <klever1987@yandex.ru>
2024-12-13 12:28:46 +0000

    rm not user from api

--- api/app/config/redis_config.py
+++
@@ -1,6 +0,0 @@
-# from redis import asyncio as aioredis
-# from .app_config import REDIS_URL
-
-
-# def get_redis():
-#     return aioredis.from_url(REDIS_URL, encoding="utf8", decode_responses=True)
