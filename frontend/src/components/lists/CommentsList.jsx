606e169d5d00494291f77245a9d7c589135af4a4
al-ov73 <klever1987@yandex.ru>
2025-01-10 05:40:30 +0000

    no comments msg

--- frontend/src/components/lists/CommentsList.jsx
+++ frontend/src/components/lists/CommentsList.jsx
@@ -4,16 +4,18 @@ import { convertDateTime } from "../../utils/utils";
 
 
 const CommentsList = ({ memeComments }) => {
-
+  if (memeComments.length === 0) {
+    return "комментариев пока нет"
+  }
   return memeComments.map((comment) => {
     const dateFormat = convertDateTime(comment.created_at)
     
-    return (memeComments && <Row key={comment.id} className="mb-3">
+    return <Row key={comment.id} className="mb-3">
       <Col>
         <h5>{comment.author_name} <small className="fs-6 fst-italic fw-light">{dateFormat}</small></h5>
         <p className="overflow-hidden">{comment.text}</p>
       </Col>
-    </Row>)
+    </Row>
   })
 }
 
