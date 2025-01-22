df99a578cb5200bcaff3849b9c5484deb0f3c22b
al-ov73 <klever1987@yandex.ru>
2025-01-22 09:02:13 +0000

    fix about msg

--- frontend/src/components/WelcomeImage.jsx
+++ frontend/src/components/WelcomeImage.jsx
@@ -2,18 +2,25 @@ import { Image } from 'react-bootstrap';
 import logo from '../static/1_description.jpeg';
 
 const WelcomeImage = () => {
-    return <div style={{ display: 'flex'}} className='align-items-center'>
+  return <div>
+      <div style={{ display: 'flex' }} className='align-items-center'>
         <Image
-            height="150rem"
-            src={logo}
-            className="rounded "
-            alt='Картинка не загрузилась:('
+          height="150rem"
+          src={logo}
+          className="rounded "
+          alt='Картинка не загрузилась:('
         />
-            <p className='mx-3 '>Мемовоз парсит весь интернет в поисках самых лучших мемов для Вас и <span className="fw-bold">каждый день</span> скидывает кучу свежайших мемов из мира IT!<br/>
-Также в нашем телеграм-канале <a href={process.env.REACT_APP_TG_LINK}>IT_мемовоз</a> Вам будет проще следить за свежим юмором!<br/>
-Там вы будете получать новые свежайшие мемы <b>каждый час!</b></p>
+        <p className='mx-3 '>Мемовоз каждый день ходит по всему интенету в поисках самых лучших мемов для Вас и <b>каждый день</b> скидывает кучу свежайших мемов из мира IT!<br />
+          Также в нашем телеграм-канале <a href={process.env.REACT_APP_TG_LINK}>IT_мемовоз</a> Вам будет проще следить за свежим юмором!<br />
+        Там вы будете получать новые свежайшие мемы <b>каждый час!</b>
+        <p className="welcome-go">ПОЕХАЛИ!</p>
+        </p><br />
+          
+      </div>
+  </div>
 
-    </div>
+
+
 
 
 }
--- frontend/src/components/modals/AboutModal.jsx
+++ frontend/src/components/modals/AboutModal.jsx
@@ -12,18 +12,18 @@ const AboutModal = ({ show, onHide }) => {
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
-              <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
+      <Modal.Header closeButton className="about-modal-header">
+        <Modal.Title id="contained-modal-title-vcenter" >
           О проекте
         </Modal.Title>
       </Modal.Header>
 
-      <Modal.Body>
+      <Modal.Body className="about-modal-body">
       Мемовоз парсит весь интернет в поисках самых лучших мемов для Вас и каждый день скидывает кучу свежайших мемов из мира IT!<br/><br/>
       Также в нашем телеграм-канале <a href="https://t.me/it_memovoz">IT_мемовоз</a> Вам будет проще следить за свежим юмором!
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={onHide}>Закрыть</Button>
+        <Button className="nav-button" onClick={onHide}>Закрыть</Button>
       </Modal.Footer>
     </Modal>
   );
