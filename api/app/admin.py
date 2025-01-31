d8c87de94803f669a2c78027692d2f1e1e6d5a73
al-ov73 <klever1987@yandex.ru>
2025-01-31 12:36:15 +0000

    add admin auth

--- api/app/admin.py
+++ api/app/admin.py
@@ -1,3 +1,4 @@
+from .utils.auth_utils import authenticate_user, get_current_user
 from sqladmin import ModelView
 from .models.user import User
 from .models.comment import Comment
@@ -6,27 +7,31 @@ from .models.like import Like
 from .models.message import Message
 from .models.meme import Meme
 from .models.visit import Visit
-
+from .config.db_config import db
 from sqladmin.authentication import AuthenticationBackend
-from starlette.requests import Request
+from fastapi import Request
+from typing import Annotated, Optional
+from fastapi import Depends, HTTPException, status, UploadFile
+from fastapi.security import OAuth2PasswordBearer
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 class AdminAuth(AuthenticationBackend):
     async def login(self, request: Request) -> bool:
         form = await request.form()
-        username, password = form["username"], form["password"]
-
-        print(username, password)
-
-        return True
+        username, password = form.get("username"), form.get("password")
+        user = authenticate_user(username, password, db)
+        if user and user.is_admin:
+            return True
+        return False
 
     async def logout(self, request: Request) -> bool:
-        # Usually you'd want to just clear the session
         request.session.clear()
         return True
 
-    async def authenticate(self, request: Request) -> bool:
-        token = request.session.get("token")
-
+    async def authenticate(self, token: Annotated[str, Depends(oauth2_scheme)]) -> bool:
+        # user = await get_current_user(token)
+        # print('user', user)
         # if not token:
         #     return False
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,20 +3,35 @@ import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
 import useAuth from '../hooks/index.js';
 import { useNavigate } from "react-router-dom";
-import React, { useState, } from "react";
+import React, { useState, useEffect } from "react";
 import ChatModal from './modals/ChatModal.jsx'
 import AboutModal from './modals/AboutModal';
-import { getUsernameFromStorage } from '../utils/utils.js';
-import { sendButtonMsgToBot } from '../utils/requests.js';
+import { getUsernameFromStorage, getUserIdFromStorage } from '../utils/utils.js';
+import { sendButtonMsgToBot, getUser } from '../utils/requests.js';
 
 
 const NavbarPage = ({ full }) => {
+  const [userIsAdmin, setUserIsAdmin] = useState(false);
   const auth = useAuth();
   const navigate = useNavigate();
   const [modalAboutShow, setModalAboutShow] = useState(false);
   const [modalChatShow, setModalChatShow] = useState(false);
+  const access_token = localStorage.getItem('user')
 
   const username = getUsernameFromStorage()
+  const userId = getUserIdFromStorage();
+
+  useEffect(() => {
+    const inner = async () => {
+      if (userId) {
+        const user = await getUser(userId, access_token);
+        if (user.is_admin) {
+          setUserIsAdmin(true)
+        }
+      }
+    }
+    inner();
+  }, [])
 
   const handleLogout = () => {
     localStorage.removeItem('user');
@@ -42,7 +57,13 @@ const NavbarPage = ({ full }) => {
       }}>
         В телеграмме проще <i className="fab fa-telegram-plane"></i>
       </Button>
-
+      {/* {userIsAdmin && <Button variant="outline-primary" className='mx-4 animated-button' onClick={(e) => {
+        e.preventDefault();
+        sendButtonMsgToBot()
+        window.location.href = `${process.env.REACT_APP_API_URL}/admin`;
+      }}>
+       Админ
+      </Button>} */}
       {full && (
         <>
           {username ? (
