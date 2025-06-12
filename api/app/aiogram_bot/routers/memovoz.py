8b80ea839ff3bbb3721d8304f0575c2bc01125c8
al-ov73 <klever1987@yandex.ru>
2025-06-12 23:13:18 +0400

    add last visits

--- api/app/aiogram_bot/routers/memovoz.py
+++ api/app/aiogram_bot/routers/memovoz.py
@@ -54,7 +54,7 @@ async def visits_callback(callback_query: types.CallbackQuery):
     visits_stat = await visit_repo.get_visit_count_by_day(db=db)
     last_visits = await visit_repo.get_last_visits(db=db)
     formated_last_visits = "\n".join(
-        [f"{visit.ip} - {visit.visit_count} визитов) - {visit.city} - {visit.visit_at.strftime('%d %b')}"
+        [f"{visit.ip} - всего {visit.visit_count} раз. - {visit.city} - {visit.visit_at.strftime('%d.%m %H:%M')}"
          for visit in last_visits]
     )
     formated_day_stat = format_visits_day_stat(visits_stat)
