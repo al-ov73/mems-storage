530d85e46d6992b43f58ae6d050c2a19b980a8ec
al-ov73 <klever1987@yandex.ru>
2025-07-14 22:19:10 +0400

    add landing to visit

--- api/app/models/visit.py
+++ api/app/models/visit.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import Mapped, mapped_column
 
 from ..config.db_config import Base
 from .user import created_at, intpk
@@ -18,3 +18,4 @@ class Visit(Base):
     city: Mapped[Optional[str]]
     visit_at: Mapped[created_at]
     is_new_user: Mapped[Optional[bool]]
+    landing_page: Mapped[Optional[str]] = mapped_column(default="memovoz.ru", nullable=True)
