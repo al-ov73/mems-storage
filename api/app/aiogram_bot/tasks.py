93e6f4eeed08abc54a23dfc1f08041ee89d96744
al-ov73 <klever1987@yandex.ru>
2025-08-14 22:24:57 +0400

    change everyday task

--- api/app/aiogram_bot/tasks.py
+++ api/app/aiogram_bot/tasks.py
@@ -1,6 +1,10 @@
 from aiogram import Bot
+from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
 
-from ..config.config import MY_API_ID
+from ..config.config import MY_API_ID, NOT_CHECKED_URL
+from ..config.db_config import db
+from ..config.dependencies import meme_repo
 
 
 async def send_reminder(bot: Bot, data: dict):
@@ -9,5 +13,11 @@ async def send_reminder(bot: Bot, data: dict):
     await bot.send_message(user_id, text)
 
 
+def notchecked_keyboard(notchecked_count: int) -> InlineKeyboardButton:
+    builder = InlineKeyboardBuilder()
+    builder.button(text=f"Проверить {notchecked_count} шт.", web_app=WebAppInfo(url=NOT_CHECKED_URL))
+    return builder.as_markup(resize_keyboard=True)
+
 async def send_day_stat(bot: Bot, stat: str):
-    await bot.send_message(MY_API_ID, stat)
+    day_stat = await meme_repo.get_published_stat(db=db)
+    await bot.send_message(MY_API_ID, stat, reply_markup=notchecked_keyboard(day_stat.not_checked))
